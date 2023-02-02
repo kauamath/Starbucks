8ac38e1e509482f3757eee7cbbf6a906887eba99
kauamath <medeiroskaua372@gmail.com>
2023-02-01 21:57:27 -0400

    finished project

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let imageStarbucks = document.querySelector("#firstImage");
+let cirle = document.querySelector(".circle");
+
+function changeImage(chanhge) {
+  imageStarbucks.src = chanhge;
+}
+
+function changeCirle(change) {
+  cirle.style.background = change;
+}
